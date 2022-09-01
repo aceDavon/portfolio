83a06299de7169aa67a2fbfe60d9da6f652ca43b
aceDavon <makojidavid@gmail.com>
2022-09-01 21:51:58 +0100

    Make API calls to fetch data
    Dispatch the fetchData action once onload to populate local state with
    remote data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import * as React from 'react'
+import { useDispatch } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
+import { fetchData } from './data/dataSlice';
+import { RepoLinks } from './data/featuredcards';
 
 function App() {
+  const dispatch = useDispatch()
+  React.useEffect(() => {
+    RepoLinks.map((x) => dispatch(fetchData(x)));
+  }, [])
   return (
     <>
       <Navbar />
