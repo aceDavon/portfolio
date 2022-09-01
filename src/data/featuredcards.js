bef54e92281228c5325a2d0c1b888dfb98b53783
aceDavon <makojidavid@gmail.com>
2022-09-01 21:53:05 +0100

    Refactor code base to use Redux
    Refactor codebase to use the reduxtoolkit store configuration for state management

--- src/data/featuredcards.js
+++ src/data/featuredcards.js
@@ -1,37 +1,40 @@
 export const FeaturedURL = [
-  'https://aceDavon.github.io/html-css-linters',
   'https://aceDavon.github.io/Portfolio-project',
+  'https://michael-duke.github.io/Capstone-II',
   'https://acedavon.github.io/Awesome-books-with-modules/',
   'https://aceDavon.github.io/todo-list',
   'https://aceDavon.github.io/LeaderBoard',
+  'https://aceDavon.github.io/html-css-linters',
 ];
 
 export const RepoLinks = [
-  'aceDavon/html-css-linters',
   'aceDavon/Portfolio-project',
+  'michael-duke/Capstone-II',
   'michael-duke/Awesome-Books',
   'aceDavon/todo-list',
   'aceDavon/LeaderBoard',
+  'aceDavon/html-css-linters',
   'acedavon/Unit-testing-JS',
 ];
 
 export const featuredRepo = [
-  'aceDavon/LeaderBoard',
+  'michael-duke/Capstone-II',
   'michael-duke/Awesome-Books',
   'aceDavon/todo-list',
 ];
 
 export const RepoImg = [
-  'https://acedavon.github.io/Portfolio-project/img/linting_tools.webp',
   'https://acedavon.github.io/Portfolio-project/img/html_css_logo.jpg',
+  'https://user-images.githubusercontent.com/84629565/187074059-173cac56-c479-4ebb-aeeb-888d98c04ab5.png',
   'https://user-images.githubusercontent.com/84629565/182920033-c8721e0a-1ace-4d2f-81b3-eb449ac697a6.png',
   'https://github.com/aceDavon/todo-list/raw/master/src/img/3-devices-black.png',
   'https://raw.githubusercontent.com/aceDavon/LeaderBoard/development/src/img/mockup/all-devices-black.png',
+  'https://acedavon.github.io/Portfolio-project/img/linting_tools.webp',
   'https://rubygarage-production-production-bucket.s3.amazonaws.com/uploads/article_image/file/3499/TDD-principle1x.png',
 ];
 
 export const FeaturedRepoImg = [
-  'https://raw.githubusercontent.com/aceDavon/LeaderBoard/development/src/img/mockup/all-devices-black.png',
+  'https://user-images.githubusercontent.com/84629565/187074059-173cac56-c479-4ebb-aeeb-888d98c04ab5.png',
   'https://user-images.githubusercontent.com/84629565/182920033-c8721e0a-1ace-4d2f-81b3-eb449ac697a6.png',
   'https://github.com/aceDavon/todo-list/raw/master/src/img/3-devices-black.png',
 ];
--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import 'tw-elements';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './pages/landing';
+import { Provider } from 'react-redux';
+import Store from './data/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={Store}>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} >
+        <Route path='/' element={<App />}>
           <Route element={<Landing />} index />
         </Route>
       </Routes>
     </BrowserRouter>
+  </Provider>
 );
